b496d004b77d1ba613c5ea53dddc112bcdbfee5d
murix <murilopontes@gmail.com>
2014-05-15 08:54:30 +0000

    all

--- bbb-packages.sh
+++ bbb-packages.sh
@@ -1,12 +1,27 @@
+#!/bin/bash
+
+PURGE=""
+INSTALL=""
+
+PURGE="$PURGE nodejs apache2.2-common gnome-icon-theme libx11-doc libopencv-dev libgl1-mesa-dri locales vim vim-runtime freepats libicu48 libflite1 libwebkitgtk-1.0-0 libavcodec53 gstreamer0.10-ffmpeg gstreamer0.10-plugins-bad gstreamer0.10-plugins-base gstreamer0.10-plugins-good libopencore-amrnb0 libopencore-amrwb0 libavcodec-extra-53"
+PURGE="$PURGE python-wicd wicd-cli wicd-curses wicd-daemon wicd-gtk"
+
+INSTALL="$INSTALL  udhcpd cape-firmware dbus python-dbus network-manager network-manager-gnome"
+INSTALL="$INSTALL  mc less ipython sshfs aptitude python-matplotlib minicom"
+INSTALL="$INSTALL  gpsd gpsd-clients python-gps"
+INSTALL="$INSTALL  xterm icewm xrdp tightvncserver lxde-core"
+INSTALL="$INSTALL  firmware-linux firmware-am335x-pm"
+INSTALL="$INSTALL  am335x-pru-package "
+INSTALL="$INSTALL  build-essential python-dev python-setuptools python-pip python-smbus"
+INSTALL="$INSTALL  libgps-dev libmodbus-dev libnl-dev libpthread-stubs0-dev libsnmp-dev libncurses5-dev libmodbus-dev"
+INSTALL="$INSTALL  dosfstools git-core kpartx u-boot-tools wget parted pv"
+
 
 # get new list of packages
 apt-get update
 
-# remove unwanted
-apt-get -y remove nodejs apache2.2-common gnome-icon-theme libx11-doc libopencv-dev libgl1-mesa-dri locales vim vim-runtime freepats libicu48 libflite1 libwebkitgtk-1.0-0 libavcodec53 gstreamer0.10-ffmpeg gstreamer0.10-plugins-bad gstreamer0.10-plugins-base gstreamer0.10-plugins-good libopencore-amrnb0 libopencore-amrwb0 libavcodec-extra-53
-
-# remove wicd
-apt-get -y remove python-wicd wicd-cli wicd-curses wicd-daemon wicd-gtk
+# purge
+apt-get -y purge $PURGE
 
 # remove any trash
 apt-get -y autoremove
@@ -14,38 +29,21 @@ apt-get -y autoremove
 # purge remain trash
 apt-get -y purge `dpkg --get-selections | grep deinstall | awk '{print $1}'`
 
-# upgrade existing packages
+# upgrade remain existing packages
 apt-get -y upgrade
 
-# other packages
-apt-get -y install udhcpd cape-firmware dbus python-dbus network-manager network-manager-gnome
-
-#
-apt-get -y install mc less ipython sshfs aptitude python-matplotlib minicom 
-apt-get -y install gpsd gpsd-clients python-gps
-
-#
-apt-get -y install xterm icewm xrdp tightvncserver lxde-core
-
-# firmware
-apt-get -y install firmware-linux firmware-am335x-pm
-
-# PRU
-apt-get -y install am335x-pru-package 
-
-# adafruit python  BBIO
-apt-get install -y build-essential python-dev python-setuptools python-pip python-smbus
-easy_install -U distribute
-pip install Adafruit_BBIO
-
-#  other libraries
-apt-get -y install libgps-dev libmodbus-dev libnl-dev libpthread-stubs0-dev libsnmp-dev libncurses5-dev libmodbus-dev
-
-# image builder
-apt-get -y install dosfstools git-core kpartx u-boot-tools wget parted pv
+# install new
+apt-get -y $INSTALL
 
 # clean download cache
 apt-get clean
+
+# install python modules
+easy_install -U distribute
+pip install Adafruit_BBIO
+
+
+
 
 
 
