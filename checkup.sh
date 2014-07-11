6fc12ce0288d7fc146b93210a3cccb26790bfe7c
murix <murilopontes@gmail.com>
2014-07-11 16:08:49 +0000

    go

--- checkup.sh
+++ checkup.sh
@@ -7,7 +7,7 @@ route add default gw 192.168.7.1
 echo nameserver 8.8.8.8 > /etc/resolv.conf
 
 # other packages
-apt-get -y install udhcpd cape-firmware mc less dbus ipython
+apt-get -y install udhcpd cape-firmware mc less dbus ipython sshfs firmware-linux am335x-pru-package firmware-am335x-pm
 
 # adafruit python  BBIO
 apt-get install -y build-essential python-dev python-setuptools python-pip python-smbus
