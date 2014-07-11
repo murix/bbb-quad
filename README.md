43b34a65736b56fe001989969f77fd40ddb3a359
murix <murilopontes@gmail.com>
2014-07-11 15:41:28 +0000

    go5

--- README.md
+++ README.md
@@ -5,15 +5,21 @@ bbb-scripts
 
 
 
+initial setup in PC
+===================
 
 
 
-
-
-
+sysctl -w net.ipv4.ip_forward=1
+iptables -t nat -A POSTROUTING -j MASQUARE
+ifconfig usb0 192.168.7.1
+minicom -c on -o -D /dev/ttyACM0
 
 initial setup in beaglebone
 ===========================
+ifconfig usb0 192.168.7.2
+route add default gw 192.168.7.1
+echo nameserver 8.8.8.8 > /etc/resolv.conf
 
 git clone git@github.com:murix/bbb-scripts.git
 
--- checkup.sh
+++ checkup.sh
@@ -1,10 +1,19 @@
 
+# network ip
 ifconfig usb0 192.168.7.2
+# network gateway
 route add default gw 192.168.7.1
+# network DNS
 echo nameserver 8.8.8.8 > /etc/resolv.conf
+
+# other packages
 apt-get -y install udhcpd cape-firmware mc less dbus ipython
+
+# adafruit python  BBIO
 apt-get install -y build-essential python-dev python-setuptools python-pip python-smbus
 easy_install -U distribute
 pip install Adafruit_BBIO
 
+#  other libraries
+apt-get -y install libgps-dev libmodbus-dev libnl-dev libpthread-stubs0-dev libsnmp-dev libncurses5-dev libmodbus-dev
 
