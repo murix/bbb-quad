e5a316700ae634990f1d8d2e0e76b088338045e1
murix <murilopontes@gmail.com>
2014-07-11 17:11:28 +0000

    go6

--- gpio-all-out-test.py
+++ gpio-all-out-test.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python
 
+from time import sleep
+import thread
 import Adafruit_BBIO.GPIO as G
+
+def function_pin_change(pin,time):
+ s=True
+ while True:
+  s= not s
+  G.output(pin,s)
+  sleep(time)
+  
 
 for p in [ "P9_", "P8_" ]:
  for i in range(1,47):
   pin=p+str(i)
-  print pin
+  print pin,
   #G.setup(pin,G.OUT,G.PUD_OFF,0)
   G.setup(pin,G.OUT,G.PUD_DOWN,0)
   #G.setup(pin,G.OUT,G.PUD_UP,0)
   #G.setup(pin,G.OUT,G.PUD_OFF,1)
   #G.setup(pin,G.OUT,G.PUD_DOWN,1)
   #G.setup(pin,G.OUT,G.PUD_UP,1)
+  func_args = ( pin, 0.5 )
+  thread.start_new_thread(function_pin_change, func_args )
+  
+
+print ""
+print "all pins change state every half second foverer"
+while 1:
+ pass
 
 
 
