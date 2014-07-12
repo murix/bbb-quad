e0ceaa7fe0738d1e441a6df16efb6663d35020ee
Murilo Pontes <murix@users.noreply.github.com>
2014-07-12 12:06:16 -0300

    Update git-config.sh

--- git-config.sh
+++ git-config.sh
@@ -1,4 +1,4 @@
 cat ~/.ssh/id_rsa.pub
-git config --global user.email "murix"
-git config --global user.name  "murilopontes@gmail.com"
+git config --global user.email "murilopontes@gmail.com"
+git config --global user.name  "murix"
 
