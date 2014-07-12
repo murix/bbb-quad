7b61ec3250f13ab7b7c35e0e3eec33db6802bc61
murix <murilopontes@gmail.com>
2014-07-12 19:18:50 +0000

    all

--- bbb-packages.sh
+++ bbb-packages.sh
@@ -1,54 +1,47 @@
 
 # get new list of packages
-#apt-get update
+apt-get update
 
-# remove unused packages
-#apt-get -y purge nodejs
-#apt-get -y purge freepats 
-#apt-get -y purge vim-runtime 
-#apt-get -y purge locales 
-#apt-get -y purge libgl1-mesa-dri 
-#apt-get -y purge libicu48 
-#apt-get -y purge libopencv-dev
-#apt-get -y purge desktop-base 
-#apt-get -y purge libruby1.9.1 
-#apt-get -y purge gnome-icon-theme 
-#apt-get -y purge apache2.2-common 
-#apt-get -y purge libavcodec-extra-53  
-#apt-get -y purge libx11-doc
-#apt-get -y purge libgtk-3-common
-
-# purge remain trash
-#apt-get -y purge `dpkg --get-selections | grep deinstall | awk '{print $1}'`
+# remove unwanted
+apt-get -y remove nodejs apache2.2-common gnome-icon-theme libx11-doc libopencv-dev libgl1-mesa-dri locales vim vim-runtime freepats libicu48 libflite1 libgtk-3-common iso-codes libwebkitgtk-1.0-0 libavcodec53 gstreamer0.10-ffmpeg gstreamer0.10-plugins-bad gstreamer0.10-plugins-base gstreamer0.10-plugins-good libopencore-amrnb0 libopencore-amrwb0 libavcodec-extra-53
 
 # remove any trash
-#apt-get -y autoremove
+apt-get -y autoremove
+
+# purge remain trash
+apt-get -y purge `dpkg --get-selections | grep deinstall | awk '{print $1}'`
 
 # upgrade existing packages
-#apt-get -y upgrade
+apt-get -y upgrade
 
 # other packages
 apt-get -y install udhcpd cape-firmware dbus python-dbus
+
 #
 apt-get -y install mc less ipython sshfs aptitude python-matplotlib
+
 #
 apt-get -y install xterm icewm xrdp tightvncserver lxde-core
+
 # firmware
 apt-get -y install firmware-linux firmware-am335x-pm
+
 # PRU
 apt-get -y install am335x-pru-package 
+
 # adafruit python  BBIO
 apt-get install -y build-essential python-dev python-setuptools python-pip python-smbus
 easy_install -U distribute
 pip install Adafruit_BBIO
+
 #  other libraries
 apt-get -y install libgps-dev libmodbus-dev libnl-dev libpthread-stubs0-dev libsnmp-dev libncurses5-dev libmodbus-dev
+
 # image builder
 apt-get -y install dosfstools git-core kpartx u-boot-tools wget parted pv
 
 # clean download cache
-#apt-get clean
+apt-get clean
 
 
 
-apt-get remove freepats libicu48 libflite1 libgtk-3-common iso-codes libwebkitgtk-1.0-0 libavcodec53 gstreamer0.10-ffmpeg gstreamer0.10-plugins-bad gstreamer0.10-plugins-base gstreamer0.10-plugins-good libopencore-amrnb0 libopencore-amrwb0 libavcodec-extra-53
