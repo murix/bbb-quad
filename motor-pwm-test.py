dc8f6449bdfd0f84a51cee45ba37ea8805c3f48d
murix <murilopontes@gmail.com>
2014-07-14 00:32:55 +0000

    all

--- motor-pwm-test.py
+++ motor-pwm-test.py
@@ -6,60 +6,78 @@ import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.UART as UART
 import Adafruit_BBIO.SPI as SPI
 import curses
+import time
 stdscr = curses.initscr()
 curses.noecho()
 
-pwm_arm=45
+PWM.cleanup()
+GPIO.cleanup()
 
-pin_fr="P8_13"
-pin_rr="P9_14"
-pin_rl="P9_21"
+# motor pin map
 pin_fl="P9_42"
+pin_fr="P8_13"
+pin_rl="P9_21"
+pin_rr="P9_14"
 
-PWM.start(pin_fl,pwm_arm,490)
-PWM.start(pin_fr,pwm_arm,490)
-PWM.start(pin_rl,pwm_arm,490)
-PWM.start(pin_rr,pwm_arm,490)
+# stay off
+GPIO.setup(pin_fl,GPIO.OUT,0)
+GPIO.setup(pin_fr,GPIO.OUT,0)
+GPIO.setup(pin_rl,GPIO.OUT,0)
+GPIO.setup(pin_rr,GPIO.OUT,0)
+time.sleep(1)
 
-#PWM.set_duty_cycle()
+# pwm to arm motor
+arm_fl=46
+arm_fr=46
+arm_rl=46
+arm_rr=46
 
-pwm_fl=pwm_arm
-pwm_fr=pwm_arm
-pwm_rl=pwm_arm
-pwm_rr=pwm_arm
+# setup PWM to arm motors
+PWM.start(pin_fl,arm_fl,490)
+PWM.start(pin_fr,arm_fr,490)
+PWM.start(pin_rl,arm_rl,490)
+PWM.start(pin_rr,arm_rr,490)
 
-while True:
-# try:
-#  stdscr.clear()
-  key = chr(stdscr.getch())
-  if key == '7': pwm_fl+=1
-  if key == '8': pwm_fl-=1
-  if key == '9': pwm_fr+=1
-  if key == '+': pwm_fr-=1
-  if key == '4': pwm_rl+=1
-  if key == '5': pwm_rl-=1
-  if key == '6': pwm_rr+=1
-  if key == '.': pwm_rr-=1
+# init PWM control vars
+pwm_fl=arm_fl
+pwm_fr=arm_fr
+pwm_rl=arm_rl
+pwm_rr=arm_rr
 
+key = 'q'
 
+def print_pwm():
+  #stdscr.clrscr()
   stdscr.addstr(0,0,"key="+key)
   stdscr.addstr(1,5,"front left (7,8)="+str(pwm_fl))
   stdscr.addstr(1,30,"front right (9,+)="+str(pwm_fr))
   stdscr.addstr(2,5,"rear left (4,5)="+str(pwm_rl))
   stdscr.addstr(2,30,"rear right (6,.)="+str(pwm_rr))
+  #stdscr.refresh()
+
+print_pwm()
+
+while True:
+  #
+  key = chr(stdscr.getch())
+  if key == '7': pwm_fl-=1
+  if key == '8': pwm_fl+=1
+  if key == '9': pwm_fr-=1
+  if key == '+': pwm_fr+=1
+  if key == '4': pwm_rl-=1
+  if key == '5': pwm_rl+=1
+  if key == '6': pwm_rr-=1
+  if key == '.': pwm_rr+=1
+  if key=='q':
+   break
+
+  print_pwm()
 
   PWM.set_duty_cycle(pin_fl,pwm_fl)
   PWM.set_duty_cycle(pin_fr,pwm_fr)
   PWM.set_duty_cycle(pin_rl,pwm_rl)
   PWM.set_duty_cycle(pin_rr,pwm_rr)
 
-
-  if key=='q':
-   break
-
-  stdscr.refresh()
-# except:
-#  pass
 
 curses.endwin()
 PWM.cleanup()
