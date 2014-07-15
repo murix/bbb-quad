ee31cc6f4b6cbd0fa341efeee33d7fc077a10d6c
murix <murilopontes@gmail.com>
2014-07-15 02:28:07 +0000

    all

--- bbb-packages.sh
+++ bbb-packages.sh
@@ -3,17 +3,28 @@
 PURGE=""
 INSTALL=""
 
-PURGE="$PURGE nodejs apache2.2-common gnome-icon-theme libx11-doc libopencv-dev libgl1-mesa-dri locales vim vim-runtime freepats libicu48 libflite1 libwebkitgtk-1.0-0 libavcodec53 gstreamer0.10-ffmpeg gstreamer0.10-plugins-bad gstreamer0.10-plugins-base gstreamer0.10-plugins-good libopencore-amrnb0 libopencore-amrwb0 libavcodec-extra-53"
+PURGE="$PURGE nodejs apache2.2-common gnome-icon-theme libx11-doc libopencv-dev"
+PURGE="$PURGE libgl1-mesa-dri locales vim vim-runtime freepats libicu48 libflite1"
+PURGE="$PURGE libwebkitgtk-1.0-0 libavcodec53 gstreamer0.10-ffmpeg gstreamer0.10-plugins-bad"
+PURGE="$PURGE gstreamer0.10-plugins-base gstreamer0.10-plugins-good libopencore-amrnb0 libopencore-amrwb0"
+PURGE="$PURGE libavcodec-extra-53"
 PURGE="$PURGE python-wicd wicd-cli wicd-curses wicd-daemon wicd-gtk"
 
-INSTALL="$INSTALL  udhcpd cape-firmware dbus python-dbus network-manager network-manager-gnome"
+INSTALL="$INSTALL  udhcpd dbus python-dbus network-manager network-manager-gnome"
 INSTALL="$INSTALL  mc less ipython sshfs aptitude python-matplotlib minicom"
+#GPS
 INSTALL="$INSTALL  gpsd gpsd-clients python-gps"
+#X11
 INSTALL="$INSTALL  xterm icewm xrdp tightvncserver lxde-core"
-INSTALL="$INSTALL  firmware-linux firmware-am335x-pm"
-INSTALL="$INSTALL  am335x-pru-package "
+#firmwares
+INSTALL="$INSTALL  firmware-linux "
+#BBB repo
+INSTALL="$INSTALL  am335x-pru-package cape-firmware device-tree-compiler firmware-am335x-pm"
+#adafruit BBIO deps
 INSTALL="$INSTALL  build-essential python-dev python-setuptools python-pip python-smbus"
+#
 INSTALL="$INSTALL  libgps-dev libmodbus-dev libnl-dev libpthread-stubs0-dev libsnmp-dev libncurses5-dev libmodbus-dev"
+#omap-image-builder deps
 INSTALL="$INSTALL  dosfstools git-core kpartx u-boot-tools wget parted pv"
 
 
