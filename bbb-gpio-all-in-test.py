0a1a3ddb0e2e3d0e6dddfdab4899f82ae7fcd64c
murix <murilopontes@gmail.com>
2014-07-15 02:06:11 +0000

    all

--- bbb-gpio-all-in-test.py
+++ bbb-gpio-all-in-test.py
@@ -6,33 +6,24 @@ import Adafruit_BBIO.GPIO as G
 import curses
 
 ss=curses.initscr()
-
 curses.noecho()
 
 G.cleanup()
 
-y=0
-x=0
 
 try:
- ss.addstr(1,1,"dasdasdasda")
+##############################
  for p in [ "P9_", "P8_" ]:
-  if p == "P9_": y=10
-  if p == "P8_": y=20
-  for i in range(1,47,2):
-   x+=1
-
-   pin1=p+str(i)
-   pin2=p+str(i+1)
-
-   G.setup(pin1,G.IN,G.PUD_OFF)
-   G.setup(pin2,G.IN,G.PUD_OFF)
-
-   ss.addstr(x,y,str(pin1))
+  for i in range(1,47):
+   pin=p+str(i)
+   ss.addstr(1,1,str(pin))
+   G.setup(pin,G.IN,G.PUD_OFF)
+###############################
 except:
  pass
 
 curses.endwin()
+print "input test end"
 
 
 
