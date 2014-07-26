940a845a88226e8e60bdc4b54956e956f4222f4f
murix <murilopontes@gmail.com>
2014-07-26 15:15:08 +0000

    all

--- quad-eclipse/build.sh
+++ quad-eclipse/build.sh
@@ -13,8 +13,11 @@ echo "build pru firmware"
 pasm -b pwm.p
 
 echo "build test program"
-g++ -g0 -O3 -Wall main.cpp pru.cpp pruPWM.cpp -lprussdrv -lpthread -o pwm-test
+g++ -g0 -O3 -Wall -std=c++0x main.cpp pru.cpp pruPWM.cpp -lprussdrv -lpthread -o pwm-test
 
+g++ -g0 -O3 -Wall -std=c++0x -lm -D_BSD_SOURCE gy86.cpp -o gy86
+
+g++ -g0 -O3 -Wall -std=c++0x baro.cpp -o baro
 
 
 
