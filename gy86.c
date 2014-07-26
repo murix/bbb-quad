ab9f9706eae9d5304f36b826afeb17efa927226f
murix <murilopontes@gmail.com>
2014-07-26 04:53:09 +0000

    all

--- gy86.c
+++ gy86.c
@@ -76,11 +76,11 @@ float hmc5883_read16(int file,uint8_t reg_msb,uint8_t reg_lsb){
 }
 
 
-void ms5611_configure(int file){
+void ms5611_configure(int file,int over,float* sp,float* salt,float* st){
     
     uint8_t buffer[3];
-    uint16_t* p16;
-    p16=(uint16_t*)buffer;
+    //uint16_t* p16;
+    //p16=(uint16_t*)buffer;
 
     if (ioctl(file,I2C_SLAVE,ADDR_MS5611) < 0) {
         printf("Failed to acquire bus access and/or talk to slave.\n");
@@ -90,37 +90,71 @@ void ms5611_configure(int file){
     buffer[0]=0x1e;
     write(file,buffer,1);
     //wait wake up
-    usleep(3000);
+    usleep(10000);
 
     buffer[0]=0xa2;
     write(file,buffer,1);
     read(file,buffer,2);
-    uint16_t c1=p16[0];
+    uint16_t c1=buffer[0]*256+buffer[1];
 
     buffer[0]=0xa4;
     write(file,buffer,1);
     read(file,buffer,2);
-    uint16_t c2=p16[0];
+    uint16_t c2=buffer[0]*256+buffer[1];
 
     buffer[0]=0xa6;
     write(file,buffer,1);
     read(file,buffer,2);
-    uint16_t c3=p16[0];
+    uint16_t c3=buffer[0]*256+buffer[1];
 
     buffer[0]=0xa8;
     write(file,buffer,1);
     read(file,buffer,2);
-    uint16_t c4=p16[0];
+    uint16_t c4=buffer[0]*256+buffer[1];
 
     buffer[0]=0xaa;
     write(file,buffer,1);
     read(file,buffer,2);
-    uint16_t c5=p16[0];
+    uint16_t c5=buffer[0]*256+buffer[1];
 
     buffer[0]=0xac;
     write(file,buffer,1);
     read(file,buffer,2);
-    uint16_t c6=p16[0];
+    uint16_t c6=buffer[0]*256+buffer[1];
+
+
+    //oversample
+    //0.065=1ms=0x40
+    //0.042=3ms=0x42
+    //0.027=4ms=0x44
+    //0.018=6ms=0x46
+    //0.012=10ms=0x48
+
+    //
+    buffer[0]=0x40+over;
+    write(file,buffer,1);
+
+    //
+    usleep(10000);
+
+    //read ADC
+    buffer[0]=0x0;
+    write(file,buffer,1);
+    read(file,buffer,3);
+    uint32_t d1=buffer[0]*65536+buffer[1]*256+buffer[2];
+
+    //
+    buffer[0]=0x50+over;
+    write(file,buffer,1);
+
+    //
+    usleep(10000);
+
+    //read ADC
+    buffer[0]=0x0;
+    write(file,buffer,1);
+    read(file,buffer,3);
+    uint32_t d2=buffer[0]*65536+buffer[1]*256+buffer[2];
 
 int debug=0;
 
@@ -132,45 +166,18 @@ if(debug){
     c5=33464;
     c6=28312;
 }
-    //oversample
-    //0.065=1ms=0x40
-    //0.042=3ms=0x42
-    //0.027=4ms=0x44
-    //0.018=6ms=0x46
-    //0.012=10ms=0x48
-
-
-
-    buffer[0]=0x48;
-    write(file,buffer,1);
-    usleep(10000);
-    buffer[0]=0x0;
-    write(file,buffer,1);
-    read(file,buffer,3);
-    uint32_t d1=buffer[0]*65536+buffer[1]*256+buffer[2];
-
-    buffer[0]=0x58;
-    write(file,buffer,1);
-    usleep(10000);
-    buffer[0]=0x0;
-    write(file,buffer,1);
-    read(file,buffer,3);
-    uint32_t d2=buffer[0]*65536+buffer[1]*256+buffer[2];
-
 if(debug){
   d1=9085466;
   d2=8569150;
 }
 
-
-
-   int32_t dt = d2 - c5*(1<<8);
-   int32_t temp = 2000 + dt*c6/(1<<23);
+   int64_t dt =( (int64_t  )d2) - (( int64_t) c5*(1<<8));
+   int64_t temp = 2000 + dt*c6/(1<<23);
 
    int64_t off= (( int64_t)  c2*(1<<16) ) + ( ( int64_t)  c4*dt/(1<<7) );
 
    int64_t sens= (( int64_t) c1*(1<<15) )+ (( int64_t)   c3*dt/(1<<8));
-   int32_t p=(d1*sens/(1<<21)-off)/(1<<15);
+   int64_t p=(d1*sens/(1<<21)-off)/(1<<15);
 
 if(debug){
 if(dt!=2366) printf("dt error\r\n");
@@ -183,23 +190,27 @@ if(p!=100009) printf("p error\r\n");
    float p0=1013.25;
    float alt = 44330.0*(1.0-pow(((p/100.0)/p0),1.0/5.255));
 
-   printf("c1=%d\r\n",c1);
-   printf("c2=%d\r\n",c2);
-   printf("c3=%d\r\n",c3);
-   printf("c4=%d\r\n",c4);
-   printf("c5=%d\r\n",c5);
-   printf("c6=%d\r\n",c6);
-   printf("d1=%d\r\n",d1);
-   printf("d2=%d\r\n",d2);
-   printf("dt=%d\r\n",dt);
-   printf("temp=%d (%f)\r\n",temp,temp/100.0);
-   printf("off=%lld\r\n",off);
-   printf("sens=%lld\r\n",sens);
-   printf("p=%d (%f)\r\n",p,p/100.0);
-   printf("p0=%f\r\n",p0);
+if(debug){
+   printf("c1=%d ",c1);
+   printf("c2=%d ",c2);
+   printf("c3=%d ",c3);
+   printf("c4=%d ",c4);
+   printf("c5=%d ",c5);
+   printf("c6=%d ",c6);
+   printf("d1=%d ",d1);
+   printf("d2=%d ",d2);
+   printf("dt=%lld ",dt);
+   printf("temp=%lld (%f) ",temp,temp/100.0);
+   printf("off=%lld ",off);
+   printf("sens=%lld ",sens);
+   printf("p=%lld (%f) ",p,p/100.0);
+   printf("p0=%f ",p0);
    printf("alt=%f\r\n",alt);
+}
 
-
+   *sp=p/100.0;
+   *salt=alt;
+   *st=temp/100.0;
 }
 
 
@@ -285,13 +296,21 @@ int main(int argc,char** argv){
     float y= to_degrees( -atan2f(ax,az) );
     float x= to_degrees( atan2f(ay,az) );
 
-    ms5611_configure(file);
+    float p,alt,tt;
+    ms5611_configure(file,8,&p,&alt,&tt);
 
-    printf("mpu6050 acc=%+3.2f %+3.2f %+3.2f temp=%+3.2f gyro=%+3.2f %+3.2f %+3.2f \r\n",ax,ay,az,tp,gx,gy,gz);
-    printf("hmc5883 mag=%+3.2f %+3.2f %+3.2f \r\n",mx,my,mz);
-    printf("ms5611 p=  \r\n");
-    printf("angles = %f %f %f\r\n",x,y,z);
-    printf("-------------------------------------------------------------------------\r\n");
+    printf("mpu6050 acc=%+3.2f %+3.2f %+3.2f temp=%+3.2f gyro=%+3.2f %+3.2f %+3.2f | ",ax,ay,az,tp,gx,gy,gz);
+    printf("hmc5883 mag=%+3.2f %+3.2f %+3.2f | ",mx,my,mz);
+    printf("angles = %f %f %f | ",x,y,z);
+    printf("ms5611 p=%f h=%f t=%f\r\n",p,alt,tt);
+
+    //ms5611_configure(file,0);
+    //ms5611_configure(file,2);
+    //ms5611_configure(file,4);
+    //ms5611_configure(file,6);
+
+
+    //printf("-------------------------------------------------------------------------\r\n");
 
   }
   return 0;
