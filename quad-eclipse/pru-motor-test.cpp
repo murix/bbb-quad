cc5d29ebe2c97a723750d6e0446539234d43efbe
murix <murilopontes@gmail.com>
2014-12-15 01:58:50 +0000

    pwm pthread

--- quad-eclipse/pru-motor-test.cpp
+++ quad-eclipse/pru-motor-test.cpp
@@ -27,6 +27,8 @@
 #include <unistd.h>
 #include <string.h>
 
+#include <pthread.h>
+
 int getch()
 {
   int            ch;
@@ -54,16 +56,7 @@ int getch()
   return ch;
 }
 
-void set_all(PRUPWM* myPWM,uint32_t* dutyns){
-	myPWM->setChannelValue(0,dutyns[0]);
-	myPWM->setChannelValue(1,dutyns[1]);
-	myPWM->setChannelValue(2,dutyns[2]);
-	myPWM->setChannelValue(3,dutyns[3]);
-	myPWM->setChannelValue(4,dutyns[4]);
-	myPWM->setChannelValue(5,dutyns[5]);
-	myPWM->setChannelValue(6,dutyns[6]);
-	myPWM->setChannelValue(7,dutyns[7]);
-}
+
 
 //
 #define PWM_HZ              400
@@ -73,37 +66,73 @@ void set_all(PRUPWM* myPWM,uint32_t* dutyns){
 #define PWM_FLY_MAX     1900000
 #define PWM_CALIB_MAX   1950000
 #define PWM_STEP           1000
+#define PWM_CHANGE      2500000
 
-uint32_t dutyns[8];
-uint32_t targetns[8];
-float errorns[8];
+typedef struct {
+ char cmd;
+ uint32_t dutyns[8];
+
+} motor_t;
 
 
-int main() {
+void *motorserver(void *arg){
+     motor_t* pdata=(motor_t*) arg;
 
+       //
        printf("enable PRU overlay\r\n");
        system("echo bone_pru0_out > /sys/devices/bone_capemgr.9/slots");
        printf("wait PRU overlay to be ready\r\n");
        usleep(1000000);
 
-
 	// Initialise PRU PWM
 	PRUPWM *myPWM = new PRUPWM(PWM_HZ);
-
 	// Start the PRU
 	myPWM->start();
 
-	//
-	for(int i=0;i<8;i++){
-           dutyns[i]=PWM_FLY_ARM;
-           targetns[i]=PWM_FLY_ARM;
-           
+        for(int ch=0;ch<8;ch++){
+	   myPWM->setChannelValue(ch,PWM_FLY_ARM);
         }
-	//
-	set_all(myPWM,dutyns);
 
- 
+	
+
+     int cache[8];
+     for(;;){
+        for(int ch=0;ch<8;ch++){
+           //cache outdated
+           if(cache[ch]!=pdata->dutyns[ch]){
+              //update cache
+              cache[ch]=pdata->dutyns[ch];
+              //safety cache
+              if(cache[ch]<PWM_FLY_ARM)   cache[ch]=PWM_FLY_ARM;
+              if(cache[ch]>PWM_CALIB_MAX) cache[ch]=PWM_CALIB_MAX;
+              //
+              myPWM->setChannelValue(ch,cache[ch]);
+           }
+        }
         
+        // wait 2,5ms for 400hz pwm complete duty
+        usleep(2500);
+        //
+        if(pdata->cmd=='a'){
+          break;
+        }
+     }
+
+        for(int ch=0;ch<8;ch++){
+	   myPWM->setChannelValue(ch,PWM_FLY_ARM);
+        }
+
+
+}
+
+
+int main() {
+
+        motor_t motors;
+        bzero(&motors,sizeof(motor_t));
+	pthread_t th1;
+	pthread_create(&th1, 0, motorserver, &motors);
+      
 
 	while(1){
 	   printf("PWM frequency %d Hz\r\n",PWM_HZ);
@@ -119,93 +148,24 @@ int main() {
 	   printf("cmd -: decrease\r\n");
 	   printf("cmd a: quit\r\n");
 	
-	   char c = getch();
-            
+	   motors.cmd = getch();
     
-	   if (c =='z'){ for(int i=0;i<8;i++) targetns[i]=PWM_FLY_ARM;   }
-	   if (c =='x'){ for(int i=0;i<8;i++) targetns[i]=PWM_FLY_MIN;   }
-	   if (c =='c'){ for(int i=0;i<8;i++) targetns[i]=PWM_FLY_MAX;   }
+	   if (motors.cmd =='z'){ for(int i=0;i<8;i++) motors.dutyns[i]=PWM_FLY_ARM;   }
+	   if (motors.cmd =='x'){ for(int i=0;i<8;i++) motors.dutyns[i]=PWM_FLY_MIN;   }
+	   if (motors.cmd =='c'){ for(int i=0;i<8;i++) motors.dutyns[i]=PWM_FLY_MAX;   }
               
-	   if (c =='s'){ for(int i=0;i<8;i++) targetns[i]=PWM_CALIB_MIN; }
-	   if (c =='d'){ for(int i=0;i<8;i++) targetns[i]=PWM_CALIB_MAX; }
+	   if (motors.cmd =='s'){ for(int i=0;i<8;i++) motors.dutyns[i]=PWM_CALIB_MIN; }
+	   if (motors.cmd =='d'){ for(int i=0;i<8;i++) motors.dutyns[i]=PWM_CALIB_MAX; }
 
-	   if (c =='+'){ for(int i=0;i<8;i++) targetns[i]+=PWM_STEP; }
-	   if (c =='-'){ for(int i=0;i<8;i++) targetns[i]-=PWM_STEP; }
-	   if (c =='a') break;
-
-	   //
-	   if (c =='1') targetns[0]+=PWM_STEP;
-	   if (c =='2') targetns[1]+=PWM_STEP;
-	   if (c =='3') targetns[2]+=PWM_STEP;
-	   if (c =='4') targetns[3]+=PWM_STEP;
-	   if (c =='5') targetns[4]+=PWM_STEP;
-	   if (c =='6') targetns[5]+=PWM_STEP;
-	   if (c =='7') targetns[6]+=PWM_STEP;
-	   if (c =='8') targetns[7]+=PWM_STEP;
-	   //
-	   if (c =='q') targetns[0]-=PWM_STEP;
-	   if (c =='w') targetns[1]-=PWM_STEP;
-	   if (c =='e') targetns[2]-=PWM_STEP;
-	   if (c =='r') targetns[3]-=PWM_STEP;
-	   if (c =='t') targetns[4]-=PWM_STEP;
-	   if (c =='y') targetns[5]-=PWM_STEP;
-	   if (c =='u') targetns[6]-=PWM_STEP;
-	   if (c =='i') targetns[7]-=PWM_STEP;
-
-
-             //           
-	     for(int i=0;i<8;i++){
-                errorns[i]=targetns[i]-dutyns[i];
-             }
-
-             float steps=100.0;
-             int step_delay_us=1;
-
-           //safety check
-           for(int k=0;k<steps;k++){
-
-	     for(int i=0;i<8;i++){
-                //limit max pwm step to prevent overload fets, battery and power supply
-                  dutyns[i]+=errorns[i]/steps;
-                  usleep(step_delay_us);
-                // minimum
-                if(dutyns[i]<PWM_FLY_ARM  || targetns[i]<PWM_FLY_ARM){
-                   dutyns[i]=PWM_FLY_ARM; 
-                   targetns[i]=PWM_FLY_ARM; 
-                }
-                // maximum
-                if(dutyns[i]>PWM_CALIB_MAX || targetns[i]>PWM_CALIB_MAX){
-                   dutyns[i]=PWM_CALIB_MAX;
-                   targetns[i]=PWM_CALIB_MAX;
-                }
-              }
-
-           printf("dutyns=|");
-	   for(int i=0;i<8;i++){
-             printf("|%d",dutyns[i]);
-           }
-	   printf("|\r\n");
-           printf("targetns=|");
-	   for(int i=0;i<8;i++){
-             printf("|%d",targetns[i]);
-           }
-	   printf("|\r\n");
-
-
-	      set_all(myPWM,dutyns);
- 
-            }
-
-	
+	   if (motors.cmd =='+'){ for(int i=0;i<8;i++) motors.dutyns[i]+=PWM_STEP; }
+	   if (motors.cmd =='-'){ for(int i=0;i<8;i++) motors.dutyns[i]-=PWM_STEP; }
+	   if (motors.cmd =='a') break;
 	}
+        
+        printf("wait motorserver terminate...\r\n");
+        pthread_join(th1, NULL);
+        printf("motorserver terminated\r\n");
+        
 
-        //Leave motors ARMED
-	for(int i=0;i<8;i++){
-             dutyns[i]=PWM_FLY_ARM;
-             targetns[i]=PWM_FLY_ARM;
-
-        }
-	//
-	set_all(myPWM,dutyns);
 }
  
