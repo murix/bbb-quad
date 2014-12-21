4d86381d1325667b324ce5fa9ee5727eb03d069c
murix <murilopontes@gmail.com>
2014-12-21 16:36:11 +0000

    fix build

--- quad-eclipse/udpserver-test.cpp
+++ quad-eclipse/udpserver-test.cpp
@@ -24,35 +24,35 @@ SOFTWARE.
 
 
 //////////////////////-----------  C Standard Library header files
-#include <assert.h>	//Conditionally compiled macro that compares its argument to zero
-#include <complex.h> //(since C99)	Complex number arithmetic
-#include <ctype.h>	//Functions to determine the type contained in character data
-#include <errno.h>	//Macros reporting error conditions
-#include <fenv.h> //(since C99)	Floating-point environment
-#include <float.h>	//Limits of float types
-#include <inttypes.h> //(since C99)	Format conversion of integer types
-#include <iso646.h> //(since C95)	Alternative operator spellings
-#include <limits.h>	//Sizes of basic types
-#include <locale.h>	//Localization utilities
+//#include <assert.h>	//Conditionally compiled macro that compares its argument to zero
+//#include <complex.h> //(since C99)	Complex number arithmetic
+//#include <ctype.h>	//Functions to determine the type contained in character data
+//#include <errno.h>	//Macros reporting error conditions
+//#include <fenv.h> //(since C99)	Floating-point environment
+//#include <float.h>	//Limits of float types
+//#include <inttypes.h> //(since C99)	Format conversion of integer types
+//#include <iso646.h> //(since C95)	Alternative operator spellings
+//#include <limits.h>	//Sizes of basic types
+//#include <locale.h>	//Localization utilities
 #include <math.h>	//Common mathematics functions
-#include <setjmp.h>	//Nonlocal jumps
-#include <signal.h>	//Signal handling
-#include <stdalign.h> //(since C11)	alignas and alignof convenience macros
-#include <stdarg.h>	//Variable arguments
-#include <stdatomic.h> //(since C11)	Atomic types
-#include <stdbool.h> //(since C99)	Boolean type
-#include <stddef.h>	//Common macro definitions
+//#include <setjmp.h>	//Nonlocal jumps
+//#include <signal.h>	//Signal handling
+//#include <stdalign.h> //(since C11)	alignas and alignof convenience macros
+//#include <stdarg.h>	//Variable arguments
+//#include <stdatomic.h> //(since C11)	Atomic types
+//#include <stdbool.h> //(since C99)	Boolean type
+//#include <stddef.h>	//Common macro definitions
 #include <stdint.h> //(since C99)	Fixed-width integer types
 #include <stdio.h>	//Input/output
 #include <stdlib.h>	//General utilities: memory management, program utilities, string conversions, random numbers
-#include <stdnoreturn.h> //(since C11)	noreturn convenience macros
+//#include <stdnoreturn.h> //(since C11)	noreturn convenience macros
 #include <string.h>	//String handling
-#include <tgmath.h> //(since C99)	Type-generic math (macros wrapping math.h and complex.h)
-#include <threads.h> //(since C11)	Thread library
-#include <time.h> //Time/date utilities
-#include <uchar.h> //(since C11)	UTF-16 and UTF-32 character utilities
-#include <wchar.h> //(since C95)	Extended multibyte and wide character utilities
-#include <wctype.h> //(since C95)	Wide character classification and mapping utilities
+//#include <tgmath.h> //(since C99)	Type-generic math (macros wrapping math.h and complex.h)
+//#include <threads.h> //(since C11)	Thread library
+//#include <time.h> //Time/date utilities
+//#include <uchar.h> //(since C11)	UTF-16 and UTF-32 character utilities
+//#include <wchar.h> //(since C95)	Extended multibyte and wide character utilities
+//#include <wctype.h> //(since C95)	Wide character classification and mapping utilities
 
 /////////////////////-------- SYSTEM LIBRARIES
 //socket
@@ -161,7 +161,7 @@ void *task_adc(void *arg){
 #define PWM_STEP_PER_CYCLE     1000
 #define PWM_NS_PER_SEC     (1000*1000*1000)
 #define PWM_CYCLE_IN_NS    (PWM_NS_PER_SEC/PWM_HZ)
-#define PWM_CYCLE_IN_US    (PWM_CYCLE_IN_NS/1000);
+#define PWM_CYCLE_IN_US    (PWM_CYCLE_IN_NS/1000)
 #define MOTOR_CMD_NORMAL    0
 #define MOTOR_CMD_CALIB_MIN 1
 #define MOTOR_CMD_CALIB_MAX 2
