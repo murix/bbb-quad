5b55e4204fb92aa431f3394f32dc0bcede297845
murix <murilopontes@gmail.com>
2014-12-27 02:08:58 +0000

    build fix

--- quad-eclipse/build.sh
+++ quad-eclipse/build.sh
@@ -3,7 +3,7 @@
 echo "---------------- device tree ------------------"
 dtc -O dtb -o bone_pru0_out-00A0.dtbo -b 0 -@ bone_pru0_out-00A0.dts
 cp -av bone_pru0_out-00A0.dtbo /lib/firmware
-pasm -b pru-pwm-fw.p pru-pwm-fw.bin
+pasm -b pru-pwm-fw.p pru-pwm-fw
 
 echo "---------------- cmake ------------------------"
 rm CMakeCache.txt
