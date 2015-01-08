7b192b3758a6a3322aa1917ca77994093679af72
murix <murilopontes@gmail.com>
2015-01-08 11:46:59 +0000

    change tasks

--- quad-cdt/src/udpserver-test.cpp
+++ quad-cdt/src/udpserver-test.cpp
@@ -1012,7 +1012,7 @@ int main(int argc,char** argv){
 	pthread_attr_setinheritsched(&attr,PTHREAD_EXPLICIT_SCHED);
 
 	//
-#if 1
+#if 0
 	pthread_attr_setschedpolicy(&attr,SCHED_FIFO);
 	struct sched_param schedParam;
 	schedParam.sched_priority=sched_get_priority_max(SCHED_FIFO);
@@ -1043,14 +1043,14 @@ int main(int argc,char** argv){
 
 
 	//
-	pthread_create(&id_adc                          , &attr, task_adc                          , &drone_data);
+	pthread_create(&id_adc                          , NULL, task_adc                          , &drone_data);
 	//pthread_create(&id_imu                          , &attr, task_imu                          , &drone_data);
-	pthread_create(&id_motors                       , &attr, task_motors                       , &drone_data);
-	pthread_create(&id_rx_joystick_and_tx_telemetric, &attr, task_rx_joystick_and_tx_telemetric, &drone_data);
-	pthread_create(&id_pilot                        , &attr, task_pilot                        , &drone_data);
-	pthread_create(&id_ps3                          , &attr, task_bluetooth_ps3                , &drone_data);
-	pthread_create(&id_gps                          , &attr, task_gps                          , &drone_data);
-	pthread_create(&id_spi                          , &attr, task_spi_cc1101                   , &drone_data);
+	pthread_create(&id_motors                       , NULL, task_motors                       , &drone_data);
+	pthread_create(&id_rx_joystick_and_tx_telemetric, NULL, task_rx_joystick_and_tx_telemetric, &drone_data);
+	pthread_create(&id_pilot                        , NULL, task_pilot                        , &drone_data);
+	pthread_create(&id_ps3                          , NULL, task_bluetooth_ps3                , &drone_data);
+	pthread_create(&id_gps                          , NULL, task_gps                          , &drone_data);
+	pthread_create(&id_spi                          , NULL, task_spi_cc1101                   , &drone_data);
 
 	/////////////////////////////////////////////////////123456789012345
 	pthread_setname_np(id_adc                          ,"adc-vbat       ");
